dffec5fb3fb13253d6b84411212343f27b034804
Garben <877048038@qq.com>
2021-12-30 19:07:35 +0800

    commit

--- src/main/java/com/graben/springbootdemo/controller/TestController.java
+++ src/main/java/com/graben/springbootdemo/controller/TestController.java
@@ -1,4 +1,14 @@
 package com.graben.springbootdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
+    @RequestMapping("/demoC")
+    @ResponseBody
+    public String demoC() {
+        return "success";
+    }
 }
